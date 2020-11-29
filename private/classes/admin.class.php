aae82921b13c58afb26a5a2a829df3402c1380ea
Miles <mileshollifieldgfp@gmail.com>
2020-11-29 01:05:19 -0500

    Finished Verify password

--- private/classes/admin.class.php
+++ private/classes/admin.class.php
@@ -31,6 +31,10 @@ class Admin extends DatabaseObject {
  protected function set_hashed_password() {
    $this->hashed_password = password_hash($this->password, PASSWORD_BCRYPT);
  }
+  
+ public function verify_password($password) {
+   return password_verify($password, $this->hashed_password);
+ } 
 
  protected function create() {
    $this->set_hashed_password();
